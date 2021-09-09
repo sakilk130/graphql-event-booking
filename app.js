b7ccc7a6d85f405fe6d8b977eb30f3b5b1b2c79d
Sakil Khan <sakilk130@gmail.com>
2021-09-09 17:48:52 +0600

    setup graphql

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const { graphqlHTTP } = require('express-graphql');
+
+const { buildSchema } = require('graphql');
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('OK');
-});
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery{
+        events: [String!]!
+    }
+
+    type RootMutation{
+        createEvent(name:String):String
+    }
+    
+    schema {
+        query:RootQuery
+        mutation:RootMutation
+    }
+    `),
+    rootValue: {
+      events: () => {
+        return ['graphql', 'Apollo', 'rest api'];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 app.listen(3000, () => {
   console.log('server is running on 3000');
