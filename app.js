670245f5a2eb4c40013e0ac810f918ce21bfc710
Sakil Khan <sakilk130@gmail.com>
2021-09-09 16:51:27 +0600

    setup project

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
+app.listen(3000, () => {
+  console.log('server is running on 3000');
+});
