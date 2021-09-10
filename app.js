cefcbaf9aabfbe408357df85f14a0451620ae714
sakilk130 <sakilk130@gmail.com>
2021-09-10 13:44:06 +0600

    create reslations and structure graphql code

--- app.js
+++ app.js
@@ -2,63 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const { graphqlHTTP } = require('express-graphql');
-
-const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const graphQlShcema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
 
 app.use(bodyParser.json());
-let events = [];
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema: buildSchema(`
-    type Event{
-      _id:ID!
-      title:String!
-      description:String!
-      price:Float!
-      date:String!
-    }
-
-    input EventInput{
-      title:String!
-      description:String!
-      price:Float!
-      date:String!
-    }
-    
-    type RootQuery{
-        events: [Event!]!
-    }
-
-    type RootMutation{
-        createEvent(eventInput:EventInput):Event
-    }
-    
-    schema {
-        query:RootQuery
-        mutation:RootMutation
-    }
-    `),
-    rootValue: {
-      events: () => {
-        return events;
-      },
-      createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: args.eventInput.date,
-        };
-        events.push(event);
-        return event;
-      },
-    },
+    schema: graphQlShcema,
+    rootValue: graphQlResolvers,
     graphiql: true,
   })
 );
 
-app.listen(3000, () => {
-  console.log('server is running on 3000');
-});
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('database connected');
+    app.listen(3000, () => {
+      console.log('server is running on 3000');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
